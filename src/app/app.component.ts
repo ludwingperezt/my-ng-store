329319b00d6f4be96ff201dc32cf6d0c67211287
Ludwing Perez <mirage232@gmail.com>
2020-08-17 17:01:34 -0600

    Clase 10: Ejemplo de uso ngFor y evento click.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-store';
+  items = ['ludwing', 'juan', 'homero'];
+
+  addItem(): void {
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index: number): void {
+    this.items.splice(index, 1);
+  }
 }
