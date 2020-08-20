58164a34b6b00569cf5cb1862fa90df85f681872
Ludwing Perez <mirage232@gmail.com>
2020-08-19 19:48:05 -0600

    Clase 15: Ciclo de vida de los componentes.

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,13 +1,51 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { 
+  Component, 
+  Input, 
+  Output, 
+  EventEmitter, 
+  OnChanges, 
+  SimpleChanges, 
+  OnInit, 
+  DoCheck, 
+  OnDestroy } from '@angular/core';
 import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
   @Input() product: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+  constructor() {
+    console.log('Constructor');
+  }
+
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   // Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+  //   // Add '${implements OnChanges}' to the class.
+  //   console.log('ngOnChanges');
+  //   console.log(changes);
+  // }
+
+  ngOnInit(): void {
+    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    // Add 'implements OnInit' to the class.
+    console.log('ngOnInit');
+  }
+
+  ngDoCheck(): void {
+    // Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    // Add 'implements DoCheck' to the class.
+    console.log('ngDoCheck');
+  }
+
+  ngOnDestroy(): void {
+    // Called once, before the instance is destroyed.
+    // Add 'implements OnDestroy' to the class.
+    console.log('ngOnDestroy');
+  }
 
   addToCart(): void {
     console.log('Agregado al carrito');
