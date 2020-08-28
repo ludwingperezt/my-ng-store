3c169b43b4117a6c86c79ef8597dcde4fa40ec9b
Ludwing Perez <mirage232@gmail.com>
2020-08-28 12:03:29 -0600

    Clase 31: Creación de ls módulos shared y core.

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
+import { HighlightDirective } from './directives/highlight/highlight.directive';
+
+@NgModule({
+  declarations: [
+    ExponentialPipe,
+    HeaderComponent,
+    FooterComponent,
+    HighlightDirective
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    ExponentialPipe,
+    HeaderComponent,
+    FooterComponent,
+    HighlightDirective
+  ]
+})
+export class SharedModule { }
