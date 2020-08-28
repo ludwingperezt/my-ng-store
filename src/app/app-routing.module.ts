ff023b11473998716d4bf187f60edc4abedd11ab
Ludwing Perez <mirage232@gmail.com>
2020-08-28 12:43:20 -0600

    Clase 31: Ejemplo de uso de guardianes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { LayoutComponent } from './layout/layout.component';
+import { AdminGuard } from './admin.guard';
 
 const routes: Routes = [
   {
@@ -35,6 +36,7 @@ const routes: Routes = [
       },
       {
         path: 'demo',
+        canActivate: [AdminGuard],
         component: DemoComponent
       }
     ]
