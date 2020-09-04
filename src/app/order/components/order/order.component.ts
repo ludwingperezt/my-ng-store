3c84396e1f580ca208c7b62abbd0787db2a67d3b
Ludwing Perez <mirage232@gmail.com>
2020-09-04 16:03:15 -0600

    Ejercicio: Arreglar carrito para agrupar productos repetidos.

--- src/app/order/components/order/order.component.ts
+++ src/app/order/components/order/order.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/product.model';
 import { CartService } from 'src/app/core/services/cart.service';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-order',
@@ -10,10 +11,29 @@ import { Observable } from 'rxjs';
 })
 export class OrderComponent implements OnInit {
 
-  products$: Observable<Product[]>;
+  // Cambié la lista de productos por una lista de Items de carrito.
+  items$: Observable<Map<Product, number>>;
 
   constructor(private cartService: CartService) {
-    this.products$ = this.cartService.cart$;
+    this.items$ = this.cartService.cart$
+    .pipe(map(products => {
+      const listItems = new Map();
+
+      products.forEach(product => {
+        // El conteo de elementos inicia en 1 porque si un producto está en la
+        // lista, es porque al menos una vez fue agregado al carrito.
+        let count = 1;
+        if (listItems.has(product)) {
+          count = listItems.get(product) + 1;
+        }
+
+        // Inserta una nueva entrada en el map o actualiza la entrada existente
+        // con el nuevo conteo
+        listItems.set(product, count);
+      });
+
+      return listItems;
+    }));
   }
 
   ngOnInit(): void {
